043b38d3194cb24ad5ec7f885214d0ddcb0b5279
Abyul <kinsng1599@gmail.com>
2021-12-17 18:04:24 +0900

    feat:Route routes interlock And end-point

--- server/app.js
+++ server/app.js
@@ -5,10 +5,22 @@ const express = require('express');
 const app = express();
 
 /* Route Config*/
+const sessionRouter = require('./routes/session')
+const usersRouter = require('./routes/users')
+const categoryRouter = require('./routes/category')
+const questionRouter = require('./routes/questions')
+const answerRouter = require('./routes/answer')
+const likeRouter = require('./routes/like')
 
 /* middleware */
 
 /* Route */
+app.use('/', sessionRouter)
+app.use('/users', usersRouter);
+app.use('/categorys', categoryRouter);
+app.use('/questions', questionRouter);
+app.use('/answers', answerRouter);
+app.use('/likes',likeRouter);
 
 /* example http server run */
 app.listen(HTTPS_PORT, () => console.log('http Server Running : http://localhost:',HTTPS_PORT))
