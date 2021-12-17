1718f1db87703e5f2f83876da7ffe219cfcafe0e
krim45 <krim45@naver.com>
2021-12-17 23:29:10 +0700

    :sparkles:feat: Sidebar 컴포넌트 작성 - 버튼 UI 구현 - 사이드바 목록은 props로 내려받아 응답받은 목록을 렌더할 수 있게 구현

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const Side = styled.ul`
+  background-color: var(--white);
+  width: 100%;
+  height: 100%;
+  padding: 2.5rem;
+`;
+
+const Item = styled.li`
+  position: relative;
+  color: var(--pure-white);
+  text-align: center;
+  font-weight: bold;
+  font-size: 1.5rem;
+  border-radius: 20px;
+  margin-bottom: 10%;
+  padding: 1rem 2rem;
+  width: 100%;
+  background-color: var(--pink);
+
+  ::before {
+    content: '';
+    position: absolute;
+    width: 2.5rem;
+    height: 2.5rem;
+    top: 20%;
+    left: 20%;
+    border-radius: 100%;
+    background-color: ${(props) => (props.selected ? '#A3250C' : 'white')};
+  }
+`;
+
+const Sidebar = (props) => {
+  const [selectedIdx, setSelectedIdx] = useState(0);
+
+  return (
+    <Side>
+      {props.list.map((title, idx) => {
+        return (
+          <Item
+            key={idx}
+            selected={idx === selectedIdx}
+            onClick={() => setSelectedIdx(idx)}
+          >
+            {title}
+          </Item>
+        );
+      })}
+    </Side>
+  );
+};
+
+export default Sidebar;
