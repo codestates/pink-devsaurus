ff8acb797583e0a78825f0e9427324ae9d5b1a45
krim45 <krim45@naver.com>
2021-12-17 15:33:28 +0700

    :art:style: 프리티어 적용 및 담당자, 작업시간 주석 추가

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,16 +1,18 @@
+// 담당자 : 김경봉 (Front-end)
+// 2021-12-17 15:10:14
+
 import React from 'react';
 import styled from 'styled-components';
-import '../App.css';
 
 const Foot = styled.footer`
   position: fixed;
   bottom: 0;
   width: 100%;
   text-align: center;
-`
+`;
 
 const Footer = () => {
-  return <Foot>ⓒ 2021 Bug Life</Foot>
+  return <Foot>ⓒ 2021 Bug Life</Foot>;
 };
 
 export default Footer;
