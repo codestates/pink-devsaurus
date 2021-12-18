7eb1c08ea8ffd03ca992084403cf0bfb5c850a44
krim45 <krim45@naver.com>
2021-12-18 11:31:45 +0700

    :bug:bug: Sidebar 컴포넌트 수정 - 레이아웃 비율 수정 - 카테고리 목록, 카테고리 이미지 뷰 구현

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,54 +1,71 @@
-// 담당자 : 김경봉 (Front-end)
-// 2021-12-17 23:38:34
-
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const Side = styled.ul`
-  flex: 2.5 0 0;
+  flex: 2 0 0;
   background-color: var(--white);
+  min-width: 350px;
   width: 100%;
   height: 100%;
   padding: 2.5rem;
 `;
 
 const Item = styled.li`
-  position: relative;
+  display: flex;
   color: var(--pure-white);
-  text-align: center;
   font-weight: bold;
   font-size: 1.5rem;
   border-radius: 20px;
   margin-bottom: 10%;
-  padding: 1rem 2rem;
+  padding: 1rem 0;
   width: 100%;
   background-color: var(--pink);
 
-  ::before {
-    content: '';
-    position: absolute;
-    width: 2.5rem;
-    height: 2.5rem;
-    top: 20%;
-    left: 20%;
-    border-radius: 100%;
-    background-color: ${(props) => (props.selected ? '#A3250C' : 'white')};
+  :hover {
+    background-color: var(--hover-pink);
   }
 `;
 
-const Sidebar = (props) => {
+const Circle = styled.div`
+  position: relative;
+  margin: 0 5%;
+  min-width: 2rem;
+  min-height: 2rem;
+  border-radius: 100%;
+  background-color: ${(props) => (props.selected ? '#A3250C' : 'white')};
+`;
+
+const Icon = styled.img`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 1.5rem;
+  height: 1.5rem;
+  object-fit: cover;
+`;
+
+const Text = styled.div`
+  width: 100%;
+  margin-left: 5%;
+  text-align: left;
+`;
+
+const Sidebar = ({ list = [] }) => {
   const [selectedIdx, setSelectedIdx] = useState(0);
 
   return (
     <Side>
-      {props.list.map((title, idx) => {
+      {list.map((item, idx) => {
         return (
           <Item
             key={idx}
-            selected={idx === selectedIdx}
             onClick={() => setSelectedIdx(idx)}
           >
-            {title}
+            <Circle selected={idx === selectedIdx}>
+              <Icon src={item.category_image} alt="카테고리 이미지"></Icon>
+            </Circle>
+            <Text>{item.category_name}</Text>
           </Item>
         );
       })}
