ad7f64be092bff99d8390fb19b02a346456ea0f9
krim45 <krim45@naver.com>
2021-12-19 13:22:12 +0700

    :art:styles: Sidebar 컴포넌트 레이아웃 수정 - 레이아웃 수정 - 이미지 아이콘 url props 변수로 변경

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -2,23 +2,21 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const Side = styled.ul`
-  flex: 2 0 0;
+  flex: 2.5 0 0;
   background-color: var(--white);
   min-width: 350px;
   width: 100%;
-  height: 100%;
-  padding: 2.5rem;
+  padding: 2% 2%;
 `;
 
 const Item = styled.li`
   display: flex;
   color: var(--pure-white);
-  font-weight: bold;
-  font-size: 1.5rem;
+  font-weight: 500;
+  font-size: 1.8rem;
   border-radius: 20px;
   margin-bottom: 10%;
   padding: 1rem 0;
-  width: 100%;
   background-color: var(--pink);
 
   :hover {
@@ -28,26 +26,26 @@ const Item = styled.li`
 
 const Circle = styled.div`
   position: relative;
-  margin: 0 5%;
-  min-width: 2rem;
-  min-height: 2rem;
+  margin-left: 10%;
+  width: 2.5rem;
+  height: 2.5rem;
   border-radius: 100%;
   background-color: ${(props) => (props.selected ? '#A3250C' : 'white')};
+  background-image: ${(props) => props.src};
 `;
 
 const Icon = styled.img`
+  width: 1.5rem;
+  height: 1.5rem;
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  width: 1.5rem;
-  height: 1.5rem;
-  object-fit: cover;
 `;
 
 const Text = styled.div`
-  width: 100%;
-  margin-left: 5%;
+  margin-left: 10%;
+  line-height: 40px;
   text-align: left;
 `;
 
@@ -61,6 +59,7 @@ const Sidebar = ({ list = [] }) => {
           <Item
             key={idx}
             onClick={() => setSelectedIdx(idx)}
+            src={item.category_image}
           >
             <Circle selected={idx === selectedIdx}>
               <Icon src={item.category_image} alt="카테고리 이미지"></Icon>
