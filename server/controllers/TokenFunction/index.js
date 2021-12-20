dcd9a657342aa3793c99756e5ff770c242aad3ac
Abyul <kinsng1599@gmail.com>
2021-12-20 19:16:28 +0900

    :sparkles:feat: isAuthorized 인증 작성

--- server/controllers/TokenFunction/index.js
+++ server/controllers/TokenFunction/index.js
@@ -4,11 +4,27 @@ const { sign,verify } = require('jsonwebtoken')
 module.exports = {
     // create AccessToken
     genarateAccessToken : (data) => {
-        return sign(data, process.env.ACCESS_SECRET, {expiresIn:"1h"});
+        return sign(data, process.env.ACCESS_SECRET, {expiresIn:"15h"});
     },
     sendAccessToken : (code, res, accessToken ,result) => {
         res.status(code)
         .cookie('pinkcookie',accessToken,{ httpOnly:true, secure:true, sameSite:'None' })
         .json({result})
     },
+    isAuthorized : (req) => {
+        let cookie;
+        try {
+            cookie = req.cookies['pinkcookie'];
+            try {
+                return verify(cookie,process.env.ACCESS_SECRET)
+                
+            } catch (err) {
+                console.log("sec_try")
+                return null;
+            }
+        } catch ( err ) {
+            console.log("first_try")
+            return null;
+        }
+    }
 }
