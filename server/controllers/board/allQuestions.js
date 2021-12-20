160e616d4105f22ebb9b551aabd0a3efabb6a5ef
Abyul <kinsng1599@gmail.com>
2021-12-21 02:19:10 +0900

    :sparkles:feat: board allQuestions Controller

--- server/controllers/board/allQuestions.js
+++ server/controllers/board/allQuestions.js
@@ -3,33 +3,68 @@ const pool = require("../../DB/mysql");
 module.exports = async (req,res) => {
     console.log('./controllers/board/allQuestions');
     try {
-        const data = pool.query(
-            'select ' +
-            'bq.BOARD_ID as board_id, bq.TITLE as title, count(lb.BOARD_ID) as likes, ' +
-            'us.USERNAME as author, bq.CREATED_DATE as created_at, bq.MODIFY_DATE as modify_at, ' +
-            'count(aw.BOARD_ID) as answer, aw.USER_ID ' +
-            'from `BOARD_QA` as bq  ' +
-            'INNER JOIN `USER` as us ' +
-            'ON bq.USER_ID = us.USER_ID ' +
-            'INNER JOIN `LIKES_BOARD` as lb ' +
-            'ON bq.BOARD_ID = lb.BOARD_ID ' +
-            'INNER JOIN `ANSWER` as aw  ' +
-            'ON bq.BOARD_ID = aw.BOARD_ID',
-            
-            function(err,result,fields){
-                if(err)
-                    console.error(err)
-                else{
-                    console.log(result[0]);
-                    res.json({result})            
-                }
+
+        const boardSql = `SELECT * FROM BOARD_QA; `
+        const likeCountSql = `SELECT COUNT(BOARD_ID) AS LIKECOUNT FROM LIKES_BOARD WHERE BOARD_ID = ?;`;
+        const answerCountSql = `SELECT COUNT(BOARD_ID) AS ANSWERCOUNT FROM ANSWER WHERE BOARD_ID = ?;`;
+        const userSql = `SELECT USERNAME FROM USER WHERE USER_ID = ?;`;
+
+        pool.query(boardSql,(err,result) => {
+            if(err){
+                console.error(err);
+                return res.status(501).json({message:"DB Query Fail"})
             }
-        )
-        
+
+            let response = [];
+            Promise.all(result.map((element,index) => {
+                const likeCountSqls = pool.format(likeCountSql,element.BOARD_ID);
+                const answerCountSqls = pool.format(answerCountSql,element.BOARD_ID);
+                const userSqls = pool.format(userSql,element.USER_ID);
+                let promise = new Promise((resolve,reject) => {
+                    pool.query(likeCountSqls + answerCountSqls + userSqls,(err,rows) => {
+                        if(err){
+                            console.error(err);
+                            return res.status(501).json({message:"DB Query Fail"})
+                        }
+                        console.log(rows)
+                        const {LIKECOUNT} = rows[0][0];
+                        const {ANSWERCOUNT} = rows[1][0];
+                        const {USERNAME} = rows[2][0];
+                        resolve({LIKECOUNT,ANSWERCOUNT,USERNAME,index});
+                    })
+                })
+                return promise.then( (queryResult) => {
+                    result[queryResult.index].board_id = result[queryResult.index].BOARD_ID;
+                    result[queryResult.index].title = result[queryResult.index].TITLE;
+                    result[queryResult.index].likes = queryResult.LIKECOUNT;
+                    result[queryResult.index].author = queryResult.USERNAME;
+                    result[queryResult.index].modify_at = result[queryResult.index].MODIFY_DATE;
+                    result[queryResult.index].answers = queryResult.ANSWERCOUNT;
+                    result[queryResult.index].answered_user_id = result[queryResult.index].SELECTED_USER_ID;
+                    delete result[queryResult.index].BOARD_ID;
+                    delete result[queryResult.index].USER_ID;
+                    delete result[queryResult.index].CATEGORY_ID;
+                    delete result[queryResult.index].TITLE;
+                    delete result[queryResult.index].CONTENT;
+                    delete result[queryResult.index].CREATED_DATE;
+                    delete result[queryResult.index].MODIFY_DATE;
+                    delete result[queryResult.index].SELECTED_BREPLY_ID;
+                    delete result[queryResult.index].SELECTED_USER_ID;
+                })
+            }))
+            .then(() => {
+                return res.status(200).json({result})
+            })
+            .catch(err => {
+                console.error(err);
+                return res.status(501).json({message:"Promise Fail"})
+            })
+
+        }) // end poll.query
 
     } catch (err) {
         console.error(err);
-        return res.status(500).json(err)
+        return res.status(500).json({message: "Internal Server Error"});
     }
 
 }
