8c66a18645453a381607afaf0bde918ce482aa94
krim45 <krim45@naver.com>
2021-12-20 16:39:07 +0700

    :art:stlye: CSS 단위기준 변경 rem -> vmax

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -2,21 +2,25 @@
 // 2021-12-17 15:10:14
 
 import React from 'react';
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const Bar = styled.div`
   display: flex;
   width: 100%;
-  height: 4rem;
+  height: 4vmax;
   align-items: center;
   border-radius: 50px;
   border: 2px solid gray;
   flex: 0.4 0 0;
-  min-width: 240px;
+  min-width: 20vmax;
 
   > span {
     margin-left: 3%;
     margin-right: 2%;
+
+    > img {
+      width: 2vmax;
+    }
   }
 `;
 
@@ -25,8 +29,9 @@ const Input = styled.input`
   height: 70%;
   margin-right: 3%;
   border: none;
-  font-size: 1rem;
+  font-size: 1vmax;
   background-color: whitesmoke;
+  white-space: nowrap;
 
   :focus-within {
     outline: transparent;
@@ -39,16 +44,19 @@ const SearchBar = () => {
     // 엔드포인트 정하고 axios로 요청
     console.log(e.target.value);
     console.log('search');
-  }
+  };
 
   return (
     <Bar>
       <span>
-        <img src='https://img.icons8.com/ios-glyphs/30/000000/search--v1.png' alt='magnifying glass'/>
+        <img
+          src="https://img.icons8.com/ios-glyphs/30/000000/search--v1.png"
+          alt="magnifying glass"
+        />
       </span>
       <Input
-        onKeyUp={(e) => e.key === 'Enter' ? searchKeyward(e) : null}
-        placeholder='검색 키워드를 입력하세요.'
+        onKeyUp={(e) => (e.key === 'Enter' ? searchKeyward(e) : null)}
+        placeholder="검색 키워드를 입력하세요."
       ></Input>
     </Bar>
   );
