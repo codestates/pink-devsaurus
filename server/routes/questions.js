ab46f1fe79900455d6f725369bd3e2c9d3a5d4bb
Abyul <kinsng1599@gmail.com>
2021-12-21 14:52:47 +0900

    :sparkles:feat: question Routes - 답변글 조회 API 경로 추가

--- server/routes/questions.js
+++ server/routes/questions.js
@@ -1,12 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Controller = require('../controllers')
+const Controller = require("../controllers");
 
 /* session Router page */
-router.get('/', Controller.board.allQuestions)
-router.get('/:boardId', Controller.board.questions)
-router.post('/', Controller.board.write)
-router.put('/:boardId', Controller.board.modify)
-router.delete('/:boardId', Controller.board.delete)
+router.get("/", Controller.board.allQuestions);
+router.get("/:boardId", Controller.board.questions);
+router.get("/answers/:boardId", Controller.board.answers);
+router.post("/", Controller.board.write);
+router.put("/:boardId", Controller.board.modify);
+router.delete("/:boardId", Controller.board.delete);
 
 module.exports = router;
