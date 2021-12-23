8ce02322cab38031b37bfb752eee9831e956f9d2
Abyul <kinsng1599@gmail.com>
2021-12-23 20:59:55 +0900

    :spakles:feat: 삭제페이지 유저 데이터 전달

--- client/src/pages/MyPage.jsx
+++ client/src/pages/MyPage.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import Profile from '../components/Profile';
-import Sidebar from '../components/Sidebar';
-import DeleteAccountModal from '../components/DeleteAccountModal';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import Profile from "../components/Profile";
+import Sidebar from "../components/Sidebar";
+import DeleteAccountModal from "../components/DeleteAccountModal";
+import axios from "axios";
 
 const Wrapper = styled.div`
   display: flex;
@@ -40,7 +40,7 @@ const SpeechBubble = styled.div`
   z-index: 999;
 
   &:before {
-    content: '';
+    content: "";
     position: absolute;
     bottom: -25px;
     left: 50%;
@@ -66,11 +66,12 @@ const MyPage = () => {
 
   useEffect(() => {
     axios
-      .get('https://pinkdevsaurus.tk/auth', {
+      .get("https://pinkdevsaurus.tk/auth", {
         withCredentials: true,
       })
       .then((res) => {
         setUserInfo(res.data.result);
+        console.log(userInfo);
       })
       .catch((err) => {
         return;
@@ -84,9 +85,9 @@ const MyPage = () => {
   return (
     <Wrapper>
       <Sidebar
-        list={[{ category_name: '나의 정보' }, { category_name: 'My Q & A' }]}
+        list={[{ category_name: "나의 정보" }, { category_name: "My Q & A" }]}
       />
-      {isOpen ? <DeleteAccountModal modalHandler={modalHandler} /> : null}
+      {isOpen ? <DeleteAccountModal userName={userInfo['username']} userId={userInfo['user_id']} modalHandler={modalHandler} /> : null}
       <Profile userInfo={userInfo} />
       <SpeechBubbleWrapper>
         <SpeechBubble>탈퇴하기</SpeechBubble>
