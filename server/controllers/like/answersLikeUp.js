b7eacc9800627080f1692887c6f8d09b0a279a1d
Abyul <kinsng1599@gmail.com>
2021-12-23 17:48:03 +0900

    :bug:bug:답변글 좋아요 에러 코드 변경

--- server/controllers/like/answersLikeUp.js
+++ server/controllers/like/answersLikeUp.js
@@ -17,46 +17,45 @@ module.exports = async (req, res) => {
   const { answerId } = req.params;
 
   // 다중 쿼리문을 사용할 경우 SQL문 끝에 ( ; ) 이 꼭 붙어야 합니다.
-  // try {
-  // const check_403_Sql = `SELECT * FROM BOARD_QA WHERE ANSWER_ID = ${answerId} AND USER_ID = ${accessTokenData.USER_ID};`;
-  // const check_404_Sql = `SELECT * FROM BOARD_QA WHERE ANSWER_ID = ${answerId};`;
-  // const check_409_Sql = `SELECT ANSWER_ID,USER_ID FROM LIKES_BOARD WHERE USER_ID = ${accessTokenData.USER_ID} AND ANSWER_ID = ${answerId};`;
+  try {
+    const check_403_Sql = `SELECT * FROM ANSWER WHERE ANSWER_ID = ${answerId} AND USER_ID = ${accessTokenData.USER_ID};`;
+    const check_404_Sql = `SELECT * FROM ANSWER WHERE ANSWER_ID = ${answerId};`;
+    // const check_409_Sql = `SELECT ANSWER_ID,USER_ID FROM LIKES_BOARD WHERE USER_ID = ${accessTokenData.USER_ID} AND ANSWER_ID = ${answerId};`;
 
-  // pool.query(check_403_Sql + check_404_Sql + check_409_Sql, (err, result) => {
-  //   if (err) {
-  //     console.error(err);
-  //     return res.status(501).json({ message: "DB Query Fail" });
-  //   }
-  //   // check_404_Sql Error
-  //   if (result[0].length > 0) {
-  //     return res.status(403).json({ message: "Forbidden" });
-  //   }
-  //   // check_404_Sql Error
-  //   if (result[1].length === 0) {
-  //     return res.status(404).json({ message: "Not found - mysql2" });
-  //   }
-  //   // check_409_Sql Error
-  //   if (result[2].length > 0) {
-  //     return res.status(403).json({ message: "Confilict" });
-  //   }
-  // });
-
-  const transaction = await models.sequelize
-    .transaction(async (t) => {
-      const created_LIKES_BOARD = await LIKES_BOARD.findOrCreate({
-        where: {
-          USER_ID: accessTokenData.USER_ID,
-          BOARD_ID: null,
-          ANSWER_ID: answerId,
-        },
-      }).then(([result, created]) => {
-        if (!created) {
-          return res.status(201).json({ message: "Already liked" });
+    pool.query(
+      check_403_Sql + check_404_Sql /*+ check_409_Sql*/,
+      (err, result) => {
+        if (err) {
+          console.error(err);
+          return res.status(501).json({ message: "DB Query Fail" });
         }
-        return res.status(201).json({ message: "ok" });
-      });
-    })
-    .catch((error) => {
-      return res.status(500).json(error);
+        // check_404_Sql Error
+        if (result[0].length > 0) {
+          return res.status(403).json({ message: "Forbidden" });
+        }
+        // check_404_Sql Error
+        if (result[1].length === 0) {
+          return res.status(404).json({ message: "Not found - mysql2" });
+        }
+        // check_409_Sql Error
+        // if (result[2].length > 0) {
+        //   return res.status(409).json({ message: "Confilict" });
+        // }
+      }
+    );
+    LIKES_BOARD.findOrCreate({
+      where: {
+        USER_ID: accessTokenData.USER_ID,
+        BOARD_ID: null,
+        ANSWER_ID: answerId,
+      },
+    }).then(([result, created]) => {
+      if (!created) {
+        return res.status(409).json({ message: "Already liked" });
+      }
+      return res.status(201).json({ message: "ok" });
     });
+  } catch (err) {
+    return res.status(500).json(error);
+  }
 };
